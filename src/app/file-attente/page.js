ec67928a441cf15e41d2ae3aa24cf877e902e487
Bienvenue Mulebo <bienvenue.mulebo@gmail.com>
2025-08-17 10:39:10 +0100

    before dashboard

--- src/app/file-attente/page.js
+++ src/app/file-attente/page.js
@@ -243,7 +243,7 @@ export default function FileAttentePage() {
           </Card>
         </div>
 
-        {/* Service Selection */}
+        {/* Tickets traites */}
         <Card>
           <CardHeader>
             <CardTitle>Tickets Traités</CardTitle>
