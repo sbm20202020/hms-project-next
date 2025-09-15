b1663054867d7f1b08b0bbe80100f2d5ba8a940d
Bienvenue Mulebo <administrator@vmi2604174.contaboserver.net>
2025-09-15 19:04:46 +0200

    Fix Bug

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,5 @@
-// import { PrismaClient } from "@prisma/client"
-// // import { withAccelerate } from '@prisma/extension-accelerate'
-// // Utilisation d'un singleton pour éviter la saturation du pool de connexions Prisma
-// const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined }
-
-// const prisma = globalForPrisma.prisma || new PrismaClient()
-
-// // prisma.$extends(withAccelerate())
-
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../generated/prisma'
 
 const connectionString = `${process.env.DATABASE_URL}`
 
